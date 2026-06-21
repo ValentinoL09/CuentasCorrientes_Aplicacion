01083cdbf99d45284e980da6d951bd30c41b0280
Valentino Lasagno <lasagnovalentino@gmail.com>
2026-06-21 00:57:04 -0300

    chore: recrear MainApp y Launcher apuntando a la vista de Ventas

---
+++ src/main/java/com/alvear/multigraficaalvear/MainApp.java
@@ -0,0 +1,31 @@
+package com.alvear.multigraficaalvear;
+
+import com.alvear.multigraficaalvear.config.DatabaseInitializer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            DatabaseInitializer.initializeDatabase();
+
+            Parent root = FXMLLoader.load(getClass().getResource("/com/alvear/multigraficaalvear/views/VentasView.fxml"));
+
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Multigrafica Alvear - Modulo Ventas");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
